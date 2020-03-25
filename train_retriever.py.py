41d71d717addad30e6981594f5702cf6aab83f57
Hao <junhao@umich.edu>
2020-03-24 23:55:58 -0400

    add predict function to return sorted paragraphs

--- train_retriever.py.py
+++ train_retriever.py.py
@@ -1,31 +1,18 @@
-import os
-import torch
-from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
-from torchvision import transforms
 import pytorch_lightning as pl
-from argparse import ArgumentParser
-from collections import OrderedDict
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from argparse import Namespace
-from transformers.data.processors.glue import MnliProcessor
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+import hashlib
 import torch
 from transformers import (
     BertModel,
     BertTokenizer,
-    BertPreTrainedModel,
-    BertConfig,
-    AdamW,
-    get_linear_schedule_with_warmup
 )
-from transformers import BertTokenizer
-from torch.utils.data import TensorDataset, RandomSampler, DataLoader, random_split
+from typing import List
+from torch.utils.data import TensorDataset, DataLoader
 from sklearn.metrics import accuracy_score
 import torch.nn.functional as F
+import numpy as np
+from copy import deepcopy
 
 ## Berts
 model_str = 'bert-base-uncased'
@@ -35,7 +22,10 @@ bert_paragraph = BertModel.from_pretrained(model_str)
 
 ## Dataloaders
 num_dat = 10
-batch_size = 5
+batch_size = 2
+max_question_len_global = 30
+max_paragraph_len_global = 30
+default_bert_emb_dim_global = 768
 
 def process_natq_clean(file_path):
     ## if not exist save processed file
@@ -46,12 +36,12 @@ def generate_natq_clean_dataloaders():
     ## convert things to data loaders
     txt = 'I am a question'
     input_question = tokenizer.encode_plus(txt, add_special_tokens=True,
-                                           max_length=30, pad_to_max_length=True,
+                                           max_length=max_question_len_global, pad_to_max_length=True,
                                            return_tensors='pt')
     inputs_paragraph = tokenizer.batch_encode_plus(['I am positve' * 3, 'I am negative' * 4, 'I am negative'],
                                                    add_special_tokens=True,
                                                    pad_to_max_length=True,
-                                                   max_length=512,
+                                                   max_length=max_paragraph_len_global,
                                                    return_tensors='pt'
                                                    )
     dataset = TensorDataset(
@@ -63,7 +53,16 @@ def generate_natq_clean_dataloaders():
         inputs_paragraph['token_type_ids'].unsqueeze(0).repeat(num_dat, 1, 1)
     )
 
-    return DataLoader(dataset, batch_size=batch_size), DataLoader(dataset, batch_size=batch_size)
+    dataset_dev = TensorDataset(
+        input_question['input_ids'].repeat(num_dat, 1),
+        input_question['attention_mask'].repeat(num_dat, 1),
+        input_question['token_type_ids'].repeat(num_dat, 1),
+        inputs_paragraph['input_ids'].unsqueeze(0).repeat(num_dat, 1, 1),
+        inputs_paragraph['attention_mask'].unsqueeze(0).repeat(num_dat, 1, 1),
+        inputs_paragraph['token_type_ids'].unsqueeze(0).repeat(num_dat, 1, 1)
+    )
+
+    return DataLoader(dataset, batch_size=batch_size), DataLoader(dataset_dev, batch_size=batch_size)
 
 
 train_dataloader, dev_dataloader = generate_natq_clean_dataloaders()
@@ -72,7 +71,7 @@ train_dataloader, dev_dataloader = generate_natq_clean_dataloaders()
 
 class BertEncoder(nn.Module):
 
-    def __init__(self, bert, max_seq_len= 30, emb_dim = 768):
+    def __init__(self, bert, max_seq_len, emb_dim = default_bert_emb_dim_global):
         super(BertEncoder, self).__init__()
         self.max_seq_len = max_seq_len
         self.emb_dim = emb_dim
@@ -94,11 +93,18 @@ class BertEncoder(nn.Module):
         return F.normalize(h_transformed)
 
 class Retriver(nn.Module):
-    def __init__(self, bert_question_encoder, bert_paragraph_encoder, emb_dim=768):
+    def __init__(self, bert_question_encoder, bert_paragraph_encoder, tokenizer,
+                 max_question_len=max_question_len_global, max_paragraph_len=max_paragraph_len_global,
+                 emb_dim=default_bert_emb_dim_global):
         super(Retriver, self).__init__()
-        self.emb_dim = emb_dim
         self.bert_question_encoder = bert_question_encoder
         self.bert_paragraph_encoder = bert_paragraph_encoder
+        self.tokenizer = tokenizer
+        self.max_question_len = max_question_len
+        self.max_paragraph_len = max_paragraph_len
+        self.emb_dim = emb_dim
+        self.cache_hash2str = {}
+        self.cache_hash2array = {}
 
     def forward(self, input_ids_question, attention_mask_question, token_type_ids_question,
                          batch_input_ids_paragraphs, batch_attention_mask_paragraphs,
@@ -119,43 +125,54 @@ class Retriver(nn.Module):
         h_paragraphs_batch = h_paragraphs_batch_reshape.reshape(batch_size, num_document, -1)
         return h_question, h_paragraphs_batch
 
-    # def training_step(self, batch):
-    #     """
-    #     batch comes in the order of question, 1 positive paragraph,
-    #     2 negative paragraphs
-    #     """
-    #     # batch
-    #     input_ids_question, attention_mask_question, token_type_ids_question, \
-    #     batch_input_ids_paragraphs, batch_attention_mask_paragraphs, \
-    #     batch_token_type_ids_paragraphs = batch
-    #
-    #     inputs = {
-    #         'input_ids_question': input_ids_question,
-    #         'attention_mask_question': attention_mask_question,
-    #         'token_type_ids_question': token_type_ids_question,
-    #         'batch_input_ids_paragraphs': batch_input_ids_paragraphs,
-    #         'batch_attention_mask_paragraphs': batch_attention_mask_paragraphs,
-    #         'batch_token_type_ids_paragraphs': batch_token_type_ids_paragraphs
-    #     }
-    #
-    #     h_question, h_paragraphs_batch = self(**inputs)
-    #     h_paragraphs_pos = h_paragraphs_batch[:, 0, :]
-    #     batch_h_paragraphs_negs = h_paragraphs_batch[:, 1:, :]
-    #
-    #     pos_loss = F.logsigmoid(torch.bmm(h_question.unsqueeze(1),
-    #                                       h_paragraphs_pos.unsqueeze(-1))).reshape(-1)
-    #     neg_loss = F.logsigmoid(torch.bmm(h_question.unsqueeze(1),
-    #                                       batch_h_paragraphs_negs.permute(0, 2, 1))).sum(-1).reshape(-1)
-    #     loss = (pos_loss + neg_loss).sum()
-    #
-    #     return loss
+    def str2hash(drlf, str):
+        return hashlib.sha224(str.encode('utf-8')).hexdigest()
 
+    def predict(self, question_str: str, batch_paragraph_strs: List[str]):
+        self.eval()
+        with torch.no_grad():
+            ## Todo: embed all unique docs, then create ranking for all questions, then find overlap with constrained ranking
+            batch_paragraph_array = np.random.random((len(batch_paragraph_strs), self.emb_dim))
+            hashes = {}
+            uncached_paragraphs = []
+            uncached_hashes = []
+            for ind, i in enumerate(batch_paragraph_strs):
+                hash = self.str2hash(i)
+                hashes[hash] = ind
+                if hash in self.cache_hash2array:
+                    batch_paragraph_array[ind,:] = deepcopy(self.cache_hash2array[hash])
+                else:
+                    uncached_paragraphs.append(i)
+                    uncached_hashes.append(hash)
+                    self.cache_hash2str[hash] = i
+            uncached_paragraph_array = self.bert_paragraph_encoder(
+                **self.tokenizer.batch_encode_plus(uncached_paragraphs,
+                     add_special_tokens=True,
+                     pad_to_max_length=True,
+                     max_length=self.max_paragraph_len,
+                     return_tensors='pt'
+                 )
+            ).detach().numpy()
+            for ind, i in enumerate(uncached_paragraph_array):
+                self.cache_hash2array[uncached_hashes[ind]] = deepcopy(i)
+                batch_paragraph_array[ind,:] = deepcopy(i)
 
+            question_array = self.bert_question_encoder(
+                **self.tokenizer.encode_plus(question_str, add_special_tokens=True,
+                                               max_length=max_question_len_global, pad_to_max_length=True,
+                                               return_tensors='pt'))
+            relevance_scores = torch.sigmoid(
+                torch.mm(torch.tensor(batch_paragraph_array, dtype=question_array.dtype), question_array.T)).reshape(-1)
+            rerank_index = torch.argsort(-relevance_scores)
+            relevance_scores_numpy = relevance_scores.detach().numpy()
+            rerank_index_numpy = rerank_index.detach().numpy()
+            reranked_paragraphs = [batch_paragraph_strs[i] for i in rerank_index_numpy]
+            reranked_relevance_scores = relevance_scores_numpy[rerank_index]
+            return reranked_paragraphs, reranked_relevance_scores, rerank_index_numpy
 
-## Todo: torch lighting to train
 class RetriverTrainer(pl.LightningModule):
 
-    def __init__(self, retriever, emb_dim=768):
+    def __init__(self, retriever, emb_dim=default_bert_emb_dim_global):
         super(RetriverTrainer, self).__init__()
         self.retriever = retriever
         self.emb_dim = emb_dim
@@ -178,21 +195,15 @@ class RetriverTrainer(pl.LightningModule):
         }
 
         h_question, h_paragraphs_batch = self(**inputs)
-        ## Todo: do dot product together in one pass with repeating questions --> faster
-        h_paragraphs_pos = h_paragraphs_batch[:, 0, :]
-        batch_h_paragraphs_negs = h_paragraphs_batch[:, 1:, :]
-        pos_dot = torch.bmm(h_question.unsqueeze(1),
-                  h_paragraphs_pos.unsqueeze(-1))
-        neg_dot = torch.bmm(h_question.unsqueeze(1),
-                                          batch_h_paragraphs_negs.permute(0, 2, 1))
-        pos_prob = F.sigmoid(pos_dot)
-        neg_prob = F.sigmoid(-1 * neg_dot)
-        all_prob = torch.cat([pos_prob, neg_prob], -1).squeeze(1)
+        batch_size, num_document, emb_dim = batch_input_ids_paragraphs.size()
 
-        pos_loss = torch.log(pos_prob).reshape(-1)
-        neg_loss = torch.log(neg_prob).sum(-1).reshape(-1)
-        loss = -1 * (pos_loss + neg_loss).sum()
+        all_dots = torch.bmm(h_question.repeat(3, 1).unsqueeze(1),
+            h_paragraphs_batch.reshape(-1, 768).unsqueeze(2)).reshape(batch_size, num_document)
+        all_prob = torch.sigmoid(all_dots)
 
+        pos_loss = - torch.log(all_prob[:, 0]).sum()
+        neg_loss = - torch.log(1 - all_prob[:, 1:]).sum()
+        loss = pos_loss + neg_loss
         return loss, all_prob
 
     def training_step(self, batch, batch_idx):
@@ -209,28 +220,30 @@ class RetriverTrainer(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         loss, all_prob = self.step_helper(batch)
-        right_prob = torch.zeros_like(all_prob)
-        right_prob[:,0] = 1.
-        ## Todo: create val_acc
-        val_acc = loss / 2
-
+        batch_size = all_prob.size()[0]
+        _, y_hat = torch.max(all_prob, 1)
+        y_true = torch.zeros(batch_size, dtype=y_hat.dtype).type_as(y_hat)
+        val_acc = torch.tensor(accuracy_score(y_true.cpu(), y_hat.cpu())).type_as(y_hat)
         return {'val_loss': loss, 'val_acc': val_acc}
 
 
     def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+        avg_val_loss = torch.stack([x['val_loss'] for x in outputs]).sum() / len(outputs)
+        avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).sum() / len(outputs)
 
-        tensorboard_logs = {'val_loss': avg_loss, 'avg_val_acc': avg_val_acc}
-        return {'avg_val_loss': avg_loss, 'progress_bar': tensorboard_logs}
+
+        tqdm_dict = {'val_acc': avg_val_acc, 'val_loss': avg_val_loss}
+
+        # show val_loss and val_acc in progress bar but only log val_loss
+        results = {
+            'progress_bar': tqdm_dict,
+            'log': {'val_acc': avg_val_acc, 'val_loss': avg_val_loss}
+        }
+        return results
 
 
     def configure_optimizers(self):
         return torch.optim.AdamW([p for p in self.parameters() if p.requires_grad])
-        # optimizer = AdamW([p for p in self.parameters() if p.requires_grad])
-        # scheduler = get_linear_schedule_with_warmup(
-        #     optimizer, num_warmup_steps=self.warmup_steps, num_training_steps=self.num_training_steps
-        # )
 
     def train_dataloader(self):
         return train_dataloader
@@ -238,59 +251,34 @@ class RetriverTrainer(pl.LightningModule):
     def val_dataloader(self):
         return dev_dataloader
 
-
 if __name__ == '__main__':
-    ## example
+    encoder_question = BertEncoder(bert_question, max_question_len_global)
+    encoder_paragarph = BertEncoder(bert_paragraph, max_paragraph_len_global)
+    ret = Retriver(encoder_question, encoder_paragarph, tokenizer)
 
-    encoder_question = BertEncoder(bert_question, 30)
-    encoder_paragarph = BertEncoder(bert_paragraph, 512)
-    # h = encoder_question(**tokenizer.encode_plus(txt, add_special_tokens=True,
-    #     max_length=encoder_question.max_seq_len, pad_to_max_length=True,
-    # return_tensors='pt'))
+    checkpoint_callback = ModelCheckpoint(
+        filepath='out/{epoch}-{val_loss:.2f}-{val_acc:.2f}',
+        save_top_k=1,
+        verbose=True,
+        monitor='val_acc',
+        mode='max'
+    )
 
-    # txt = 'I am a question'
-    # input_question = tokenizer.encode_plus(txt, add_special_tokens=True,
-    #     max_length=30, pad_to_max_length=True,
-    # return_tensors='pt')
-    # inputs_paragraph = tokenizer.batch_encode_plus(['I am positve' * 3, 'I am negative' * 4, 'I am negative'],
-    #                                      add_special_tokens=True,
-    #                                      pad_to_max_length=True,
-    #                                      max_length=512,
-    #                                      return_tensors='pt'
-    #                                      )
-    # train_dataset = TensorDataset(
-    #     input_question['input_ids'].repeat(1, 1),
-    #     input_question['attention_mask'].repeat(1, 1),
-    #     input_question['token_type_ids'].repeat(1, 1),
-    #     inputs_paragraph['input_ids'].unsqueeze(0).repeat(1, 1, 1),
-    #     inputs_paragraph['attention_mask'].unsqueeze(0).repeat(1, 1, 1),
-    #     inputs_paragraph['token_type_ids'].unsqueeze(0).repeat(1, 1, 1)
-    # )
-    #
-    # inputs = {
-    #     'input_ids_question': input_question['input_ids'],
-    #     'attention_mask_question': input_question['attention_mask'],
-    #     'token_type_ids_question': input_question['token_type_ids'],
-    #     'batch_input_ids_paragraphs': inputs_paragraph['input_ids'],
-    #     'batch_attention_mask_paragraphs': inputs_paragraph['attention_mask'],
-    #     'batch_token_type_ids_paragraphs': inputs_paragraph['token_type_ids']
-    # }
+    early_stopping = EarlyStopping('val_acc', mode='max')
 
-    ret = Retriver(encoder_question, encoder_paragarph)
+    trainer = pl.Trainer(
+        gpus=1,
+        val_check_interval=0.1,
+        min_epochs=1, max_epochs=10,
+        checkpoint_callback=checkpoint_callback,
+        early_stop_callback=early_stopping)
+
+
     ret_trainee = RetriverTrainer(ret)
-    trainer = pl.Trainer(gpus=1)
+
     trainer.fit(ret_trainee)
 
-    # h_q, h_ps = ret(**inputs)
-    # print(h.shape, h_q.shape, h_ps.shape)
-
-
-
-    # train_dataloader = DataLoader(train_dataset, batch_size=10)
-    #
-    # for batch in train_dataloader:
-    #     pass
-    #
-    # loss = ret.training_step(batch)
-    # print(loss)
-
+    # reranked_paragraphs, reranked_relevance_scores, rerank_index = ret.predict('I am beautiful lady?', ['You are a pretty girl',
+    #                                                                                                'apple is tasty',
+    #                                                                                                'He is a handsome boy'])
+    # print(reranked_paragraphs, reranked_relevance_scores, rerank_index)
